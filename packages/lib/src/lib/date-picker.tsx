4f46c70ad9312f8a0eca3cd7abddc3198ca4691f
afeiship <1290657123@qq.com>
2024-08-04 10:42:45 +0800

    fix: date-picker

--- packages/lib/src/lib/date-picker.tsx
+++ packages/lib/src/lib/date-picker.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import noop from '@jswork/noop';
 import { DatePicker, DatePickerProps } from 'antd';
 import cx from 'classnames';
+import dayjs from 'dayjs';
 
 const CLASS_NAME = 'ac-date-picker';
 const STD_FORMAT = 'YYYY-MM-DD HH:mm:ss';
-
 type StdEventTarget = { target: { value: any } };
 type StdCallback = (inEvent: StdEventTarget) => void;
 
@@ -17,44 +16,47 @@ type Props = {
   onChange?: StdCallback;
 } & DatePickerProps;
 
+const DATA_FORMAT_HOOKS = {
+  date: 'YYYY-MM-DD',
+  datetime: 'YYYY-MM-DD HH:mm:ss',
+  time: 'HH:mm:ss',
+};
+
 export class AcDatePicker extends React.Component<Props> {
   static displayName = CLASS_NAME;
   static formSchema = CLASS_NAME;
   static defaultProps: Props = {
-    onChange: noop,
-    format: STD_FORMAT
+    format: STD_FORMAT,
   };
 
-  get values() {
-    const { value, defaultValue } = this.props;
-    let props = {};
-    if (value) props['value'] = this.parse(value);
-    if (defaultValue) props['defaultValue'] = this.parse(defaultValue);
-    return props;
+  get valueFormat() {
+    const { format } = this.props;
+    return DATA_FORMAT_HOOKS[format as 'string'] || format;
   }
 
-  handleChange = (inEvent) => {
+
+  get value() {
+    const { value } = this.props;
+    const fmt = this.valueFormat;
+
+    if (value == null || value === '') return null;
+    if (typeof value === 'string') return dayjs(value, fmt);
+    return value;
+  }
+
+  handleChange = (value: Date) => {
     const { onChange } = this.props;
-    onChange!({ target: { value: this.stringify(inEvent) } });
-  };
-
-  parse = (inValue) => {
-    const { format, dayjs } = this.props;
-    return dayjs(inValue, format as string);
-  };
-
-  stringify = (inValue) => {
-    if (!inValue) return null;
-    const { format } = this.props;
-    return inValue.format(format as string);
+    const _value = value ? dayjs(value).format(this.valueFormat) : '';
+    const _event = { target: { value: _value } };
+    onChange?.(_event);
   };
 
   render() {
-    const { className, defaultValue, value, onChange, ...props } = this.props;
-    const rest = { ...props, ...this.values };
+    const { className, format, value, onChange, ...rest } = this.props;
+
 
     return (
-      <DatePicker className={cx(CLASS_NAME, className)} onChange={this.handleChange} {...rest} />
+      <DatePicker className={cx(CLASS_NAME, className)} value={this.value} onChange={this.handleChange} {...rest} />
     );
   }
 }
