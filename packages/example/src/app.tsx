0af6196f2fd364126a71de78ed708c92fc0abd62
aric.zheng <1290657123@qq.com>
2025-10-03 07:19:12 +0800

    feat: upt docs

---
+++ packages/example/src/app.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="debug container mx-auto my-10 bg-gray-200 p-5" data-role="app-container">
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <img src="/static/images/vite.svg" alt="Vite" />
+    </div>
+  );
+}
---
+++ packages/example/src/assets/styles/tailwind.config.js
@@ -0,0 +1,16 @@
+/**
+ * @Author: aric 1290657123@qq.com
+ * @Date: 2025-02-12 11:29:58
+ * @LastEditors: aric 1290657123@qq.com
+ * @LastEditTime: 2025-02-12 13:35:39
+ */
+import jswPresets from '@jswork/presets-tailwind';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  presets: [jswPresets()],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
