0543dba1bdcba809ec31efd0dd3c8a905447cecb
aric.zheng <1290657123@qq.com>
2025-10-08 23:31:03 +0800

    feat: data source set/get

--- packages/example/src/components/TableDemo.tsx
+++ packages/example/src/components/TableDemo.tsx
@@ -1,8 +1,9 @@
 import { AcTable } from '@jswork/antd-components';
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 const Anonymous: FC = () => {
   // GET https://jsonplaceholder.typicode.com/posts?_page=1&_limit=10
+  const [dataSource, setDataSource] = useState([]);
   const fetchData = async (params) => {
     const response = await fetch(
       `https://jsonplaceholder.typicode.com/posts?_page=${params.current}&_limit=${params.pageSize}`,
@@ -13,13 +14,13 @@ const Anonymous: FC = () => {
 
   useEffect(() => {
     fetchData({ current: 1, pageSize: 10 }).then((data) => {
-      console.log(data);
+      setDataSource(data.data);
     });
   }, []);
 
   return (
     <div className="text-red-100">
-      <AcTable rowKey="id" />
+      <AcTable rowKey="id" dataSource={dataSource} />
     </div>
   );
 };
