1849944dce2d857eb125a20cfddd439d6159f18c
aric.zheng <1290657123@qq.com>
2025-10-16 14:34:38 +0800

    fix: code flask use fn

--- packages/lib/src/lib/codeflask.tsx
+++ packages/lib/src/lib/codeflask.tsx
@@ -4,14 +4,19 @@ import ReactCodeflask from '@jswork/react-codeflask';
 
 const CLASS_NAME = 'ac-codeflask';
 
-export class AcCodeFlask extends React.Component {
-  static displayName = CLASS_NAME;
-  static formSchema = CLASS_NAME;
-  static defaultProps = {
-    onChange: noop
-  };
-
-  render() {
-    return <ReactCodeflask {...this.props} />;
-  }
+export interface AcCodeFlaskProps {
+  onChange?: (code: string) => void;
+  value?: string;
+  height?: number;
+  [key: string]: any; // 兜底
 }
+
+export const AcCodeFlask: React.FC<AcCodeFlaskProps> = ({ onChange = noop, ...props }) => {
+  return <ReactCodeflask onChange={onChange} {...props} />;
+};
+
+AcCodeFlask.displayName = CLASS_NAME;
+// 如果需要 formSchema，可以挂载到函数组件上
+(AcCodeFlask as any).formSchema = CLASS_NAME;
+
+export default AcCodeFlask;
