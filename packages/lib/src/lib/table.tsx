e01e07d06f158d6cefde38ff52003ee8b6981572
aric.zheng <1290657123@qq.com>
2025-10-24 12:58:12 +0800

    feat: upt id edit

--- packages/lib/src/lib/table.tsx
+++ packages/lib/src/lib/table.tsx
@@ -2,7 +2,7 @@
  * @Author: aric 1290657123@qq.com
  * @Date: 2025-10-03 07:11:26
  * @LastEditors: aric 1290657123@qq.com
- * @LastEditTime: 2025-10-24 11:32:52
+ * @LastEditTime: 2025-10-24 12:57:50
  */
 import type { EventMittNamespace } from '@jswork/event-mitt';
 import { ReactHarmonyEvents } from '@jswork/harmony-events';
@@ -211,7 +211,7 @@ export class AcTable extends React.Component<AcTableProps, AcTableState> {
 
   public edit = (item: any) => {
     const { module, name, rowKey } = this.props;
-    nx.$nav?.(`/${module}/${name}/edit/${item[rowKey as string]}`);
+    nx.$nav?.(`/${module}/${name}/${item[rowKey as string]}/edit`);
   };
 
   /* ----- public eventBus methods end  ----- */
