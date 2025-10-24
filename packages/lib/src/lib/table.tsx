aea80990048c23e7ae9004defeb7cb52c01d3c3c
aric.zheng <1290657123@qq.com>
2025-10-24 21:10:51 +0800

    fix: rm msg

--- packages/lib/src/lib/table.tsx
+++ packages/lib/src/lib/table.tsx
@@ -2,7 +2,7 @@
  * @Author: aric 1290657123@qq.com
  * @Date: 2025-10-03 07:11:26
  * @LastEditors: aric 1290657123@qq.com
- * @LastEditTime: 2025-10-24 21:08:28
+ * @LastEditTime: 2025-10-24 21:10:47
  */
 import type { EventMittNamespace } from '@jswork/event-mitt';
 import { ReactHarmonyEvents } from '@jswork/harmony-events';
@@ -168,7 +168,6 @@ export class AcTable extends React.Component<AcTableProps, AcTableState> {
           total: 0,
         });
       }
-      void message.error(error?.message || error?.toString?.());
     } finally {
       if (!abortController.signal.aborted) {
         this.setState({ isLoading: false });
