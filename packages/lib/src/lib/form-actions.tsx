618109d9fc76061a8c5b1afb363a4ec471dafd0f
aric.zheng <1290657123@qq.com>
2025-10-25 21:51:53 +0800

    feat: add form-actions

--- packages/lib/src/lib/form-actions.tsx
+++ packages/lib/src/lib/form-actions.tsx
@@ -2,13 +2,14 @@
  * @Author: aric 1290657123@qq.com
  * @Date: 2025-10-25 18:48:19
  * @LastEditors: aric 1290657123@qq.com
- * @LastEditTime: 2025-10-25 18:57:40
+ * @LastEditTime: 2025-10-25 21:51:46
  */
 import React, { RefObject } from 'react';
 import { Button, ButtonProps, Space, SpaceProps } from 'antd';
 
 export type FormActionsProps = SpaceProps & {
   lang?: string;
+  actions?: string[];
   okText?: string;
   cancelText?: string;
   onOk?: (e: React.MouseEvent<HTMLElement>) => void;
@@ -20,6 +21,7 @@ export type FormActionsProps = SpaceProps & {
 
 const defaultProps: FormActionsProps = {
   lang: 'zh-CN',
+  actions: ['ok', 'cancel'],
 };
 
 const locales = {
@@ -36,6 +38,7 @@ const locales = {
 export const FormActions = React.forwardRef((props: FormActionsProps, ref: RefObject<HTMLDivElement>) => {
   const {
     lang,
+    actions,
     okText,
     cancelText,
     onOk,
@@ -45,25 +48,31 @@ export const FormActions = React.forwardRef((props: FormActionsProps, ref: RefOb
     buttonProps,
     ...rest
   } = { ...defaultProps, ...props };
+
   const t = (key: string) => locales[lang!][key];
+  const items = {
+    ok: <Button
+      key="ok"
+      htmlType="submit"
+      type="primary"
+      onClick={onOk}
+      children={okText || t('ok')}
+      {...buttonProps}
+      {...okProps}
+    />,
+    cancel: <Button
+      key="cancel"
+      htmlType="reset"
+      onClick={onCancel}
+      children={cancelText || t('cancel')}
+      {...buttonProps}
+      {...cancelProps}
+    />,
+  };
 
   return (
     <Space {...rest} ref={ref}>
-      <Button
-        htmlType="submit"
-        type="primary"
-        onClick={onOk}
-        children={okText || t('ok')}
-        {...buttonProps}
-        {...okProps}
-      />
-      <Button
-        htmlType="reset"
-        onClick={onCancel}
-        children={cancelText || t('cancel')}
-        {...buttonProps}
-        {...cancelProps}
-      />
+      {actions!.map(key => items[key])}
     </Space>
   );
 });
