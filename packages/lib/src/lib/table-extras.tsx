414781ba888845e807b4401bed06ea3c76efbfcd
aric.zheng <1290657123@qq.com>
2025-10-26 14:48:28 +0800

    feat: table extras

--- packages/lib/src/lib/table-extras.tsx
+++ packages/lib/src/lib/table-extras.tsx
@@ -2,12 +2,12 @@
  * @Author: aric 1290657123@qq.com
  * @Date: 2025-10-24 20:40:55
  * @LastEditors: aric 1290657123@qq.com
- * @LastEditTime: 2025-10-24 21:04:29
+ * @LastEditTime: 2025-10-26 14:48:21
  */
-import { Button, Space, SpaceProps } from 'antd';
+import { Space, SpaceProps } from 'antd';
 import React, { FC } from 'react';
-import { PlusOutlined, ReloadOutlined } from '@ant-design/icons';
 import nx from '@jswork/next';
+import { BtnBack, BtnCreate, BtnRefresh } from './button';
 
 declare global {
   interface NxStatic {
@@ -23,17 +23,6 @@ export type AcTableExtrasProps = SpaceProps & {
   actions?: string []
 }
 
-const locales = {
-  'zh-CN': {
-    reset: '刷新',
-    add: '添加',
-  },
-  'en-US': {
-    reset: 'Refresh',
-    add: 'Create',
-  },
-};
-
 const defaultExtras = {
   lang: 'zh-CN',
   actions: ['reset', 'add'],
@@ -41,17 +30,14 @@ const defaultExtras = {
 
 export const AcTableExtras: FC<AcTableExtrasProps> = (props) => {
   const { name, lang, as, asProps, actions } = { ...defaultExtras, ...props };
-  const t = (key: string) => locales[lang][key];
   const handleRefresh = () => nx.$event?.emit?.(`${name}:reset`);
   const handleAdd = () => nx.$event?.emit?.(`${name}:add`);
+  const handleBack = () => history.back();
   const AsComponent = as || Space;
   const items = {
-    reset: <Button key="reset" size="small" icon={<ReloadOutlined />} onClick={handleRefresh}>
-      {t('reset')}
-    </Button>,
-    add: <Button key="add" size="small" icon={<PlusOutlined />} onClick={handleAdd}>
-      {t('add')}
-    </Button>,
+    reset: <BtnRefresh lang={lang} onClick={handleRefresh} />,
+    add: <BtnCreate lang={lang} onClick={handleAdd} />,
+    back: <BtnBack lang={lang} onClick={handleBack} />,
   };
 
   return (
