ca4bdc0b2da90b1ca82314ff1208ffa58893cfa8
aric.zheng <1290657123@qq.com>
2025-10-29 13:32:42 +0800

    fix: optional emit

--- packages/lib/src/lib/table-extra-search.tsx
+++ packages/lib/src/lib/table-extra-search.tsx
@@ -2,7 +2,7 @@
  * @Author: aric.zheng 1290657123@qq.com
  * @Date: 2025-10-29 10:54:41
  * @LastEditors: aric.zheng 1290657123@qq.com
- * @LastEditTime: 2025-10-29 13:13:10
+ * @LastEditTime: 2025-10-29 13:31:47
  */
 import React, { FC } from 'react';
 import { AcSearch, AcSearchProps } from './search';
@@ -52,7 +52,7 @@ export const AcTableExtraSearch: FC<AcTableExtraSearchProps> = (props) => {
       placeholder={t('placeholder')}
       onSearch={(e) => {
         const q = e.target.value;
-        nx.$event.emit(`${name}:load`, { page: 1, ...defaultParams, [queryKey]: q });
+        nx.$event?.emit?.(`${name}:load`, { page: 1, ...defaultParams, [queryKey]: q });
       }}
       {...rest}
     />
