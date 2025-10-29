8e92274ec0ea53f76e258608abbc9e0888eee12e
aric.zheng <1290657123@qq.com>
2025-10-29 14:29:18 +0800

    fix: udpate rowKey status default-value

--- packages/lib/src/lib/table-status-switcher.tsx
+++ packages/lib/src/lib/table-status-switcher.tsx
@@ -2,7 +2,7 @@
  * @Author: aric.zheng 1290657123@qq.com
  * @Date: 2025-10-29 14:09:01
  * @LastEditors: aric.zheng 1290657123@qq.com
- * @LastEditTime: 2025-10-29 14:23:52
+ * @LastEditTime: 2025-10-29 14:29:05
  */
 import ReactAntStatusSwitch from '@jswork/react-ant-status-switch';
 import React, { FC } from 'react';
@@ -18,22 +18,23 @@ declare global {
 export type AcTableStatusSwitcherProps = ReactAntStatusSwitchProps & {
   name: string;
   items: any[];
+  model: any;
   statusKey?: string;
   rowKey?: string;
-  model?: any;
   statusUpdateApi?: string;
 };
 
 const defaultProps = {
   items: [],
   rowKey: 'id',
+  status: 'status',
 };
 
 export const AcTableStatusSwitcher: FC<AcTableStatusSwitcherProps> = (props) => {
-  const { name, items, statusKey, rowKey, model, statusUpdateApi, ...rest } = { ...defaultProps, ...props };
+  const { name, items, model, statusKey, rowKey, statusUpdateApi, ...rest } = { ...defaultProps, ...props };
   const _apiPath = statusUpdateApi || `${name}_update`;
   const handleStatusChange = (e) => {
-    const id = model[rowKey];
+    const id = model.get(rowKey!);
     const status = e.target.value;
     const payload = { id, [statusKey!]: status };
     nx.$event.emit(`${name}:draft`, { ...model, status });
