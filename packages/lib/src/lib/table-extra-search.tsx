b2845cb350849cf9bad8fa7afee1bba266568298
aric.zheng <1290657123@qq.com>
2025-10-29 11:07:18 +0800

    fix: ac-search no name

--- packages/lib/src/lib/table-extra-search.tsx
+++ packages/lib/src/lib/table-extra-search.tsx
@@ -2,7 +2,7 @@
  * @Author: aric.zheng 1290657123@qq.com
  * @Date: 2025-10-29 10:54:41
  * @LastEditors: aric.zheng 1290657123@qq.com
- * @LastEditTime: 2025-10-29 11:05:07
+ * @LastEditTime: 2025-10-29 11:07:07
  */
 import React from 'react';
 import { AcSearch } from './search';
@@ -25,7 +25,6 @@ export const AcTableExtraSearch: FC<AcTableExtraSearchProps> = (props) => {
   const { name, queryKey = 'keywords', ...rest } = props;
   return (
     <AcSearch
-      name={name}
       onSearch={(e) => {
         const q = e.target.value;
         nx.$event.emit(`${name}:load`, { [queryKey]: q });
