b2ef5785295feaa8082899f2f6cfb22056d464e3
aric.zheng <1290657123@qq.com>
2025-10-31 13:56:18 +0800

    feat: add reset/refresh

--- packages/lib/src/lib/card-extras.tsx
+++ packages/lib/src/lib/card-extras.tsx
@@ -2,7 +2,7 @@
  * @Author: aric 1290657123@qq.com
  * @Date: 2025-10-24 20:40:55
  * @LastEditors: aric 1290657123@qq.com
- * @LastEditTime: 2025-10-31 08:32:21
+ * @LastEditTime: 2025-10-31 13:56:09
  */
 import { Space, SpaceProps } from 'antd';
 import React, { FC, Fragment } from 'react';
@@ -32,12 +32,14 @@ const defaultExtras = {
 
 export const AcCardExtras: FC<AcCardExtrasProps> = (props) => {
   const { name, lang, as, extraBefore, extraAfter, asProps, actions } = { ...defaultExtras, ...props };
-  const handleRefresh = () => nx.$event?.emit?.(`${name}:reset`);
+  const handleRefresh = () => nx.$event?.emit?.(`${name}:refresh`);
+  const handleReset = () => nx.$event?.emit?.(`${name}:reset`);
   const handleAdd = () => nx.$event?.emit?.(`${name}:add`);
   const handleBack = () => history.back();
   const AsComponent = as || Space;
   const items = {
     refresh: <BtnRefresh lang={lang} onClick={handleRefresh} />,
+    reset: <BtnRefresh lang={lang} onClick={handleReset} />,
     add: <BtnCreate lang={lang} onClick={handleAdd} />,
     back: <BtnBack lang={lang} onClick={handleBack} />,
   };
